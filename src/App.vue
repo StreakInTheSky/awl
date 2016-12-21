5ef095adbcf43ed608f6111be80c1f88285620dd
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-12-20 21:21:04 -0800

    refectored header component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <div id="app">
-    <header>
-      <router-link to="/" class="logo">
-        <h1>Art with Larisse</h1>
-      </router-link>
-      <nav>
-        <ul class="nav__list">
-          <li class="nav__list--item"><router-link to="/art-classes">Art Classes</router-link></li>
-          <li class="nav__list--item"><router-link to="/locations">Locations</router-link></li>
-          <li class="nav__list--item"><router-link to="/art-camps">Art Camps</router-link></li>
-          <li class="nav__list--item"><router-link to="/art-parties">Art Parties</router-link></li>
-          <li class="nav__list--item"><router-link to="/tuition">Tuition</router-link></li>
-          <li class="nav__list--item"><router-link to="/about-us">About Us</router-link></li>
-        </ul>
-      </nav>
-    </header>
+    <HeaderComponent />
     <main>
       <router-view></router-view>
     </main>
@@ -23,61 +9,22 @@
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
 export default {
   name: 'app',
-  components: { FooterComponent },
+  components: { HeaderComponent, FooterComponent },
 };
 </script>
 
 <style lang="scss">
   @import 'assets/scss/app';
   @import 'assets/scss/variables';
+
   body { background: url('assets/img/background-tile.jpg') $brand-primary }
   #app {
     max-width: 1480px;
     margin: 0 auto;
-  }
-  header {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    flex-flow: column nowrap;
-    color: $brand-grey;
-    background-color: $brand-grey-light;
-    -webkit-font-smoothing: antialiased;
-    height: 250px;
-  }
-  .logo {
-    content: url('assets/img/logo.png');
-    width: 465px;
-    height: 83px;
-    margin-top: 20px;
-  }
-  .nav {
-    &__list {
-      font-family: $font-primary;
-      text-transform: uppercase;
-      font-weight: bold;
-      display: flex;
-      justify-content: space-between;
-      margin-bottom: 15px;
-
-      &--item {
-        display: inline-block;
-
-        a {
-          text-decoration: none;
-          color: inherit;
-          white-space: nowrap;
-          padding: 15px 2.5vw;
-
-          &:hover, .router-link-active {
-            color: $brand-primary;
-          }
-        }
-      }
-    }
   }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <header>
+    <router-link to="/" class="logo">
+      <h1>Art with Larisse</h1>
+    </router-link>
+    <nav>
+      <ul class="nav__list">
+        <li class="nav__list--item"><router-link to="/art-classes">Art Classes</router-link></li>
+        <li class="nav__list--item"><router-link to="/locations">Locations</router-link></li>
+        <li class="nav__list--item"><router-link to="/art-camps">Art Camps</router-link></li>
+        <li class="nav__list--item"><router-link to="/art-parties">Art Parties</router-link></li>
+        <li class="nav__list--item"><router-link to="/tuition">Tuition</router-link></li>
+        <li class="nav__list--item"><router-link to="/about-us">About Us</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+  @import '../assets/scss/variables';
+
+  header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-flow: column nowrap;
+    color: $brand-grey;
+    background-color: $brand-grey-light;
+    -webkit-font-smoothing: antialiased;
+    height: 250px;
+
+    .logo {
+      content: url('../assets/img/logo.png');
+      width: 465px;
+      height: 83px;
+      margin-top: 20px;
+    }
+    .nav {
+      &__list {
+        font-family: $font-primary;
+        text-transform: uppercase;
+        font-weight: bold;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 15px;
+
+        &--item {
+          display: inline-block;
+
+          a {
+            text-decoration: none;
+            color: inherit;
+            white-space: nowrap;
+            padding: 15px 2.5vw;
+
+            &:hover, .router-link-active {
+              color: $brand-primary;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
