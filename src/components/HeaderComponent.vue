7ef0315bff9711d9281de7e5401e0d4a33211526
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-12-20 22:10:19 -0800

    WIP Home hero responsive styles

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -22,7 +22,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
   @import '../assets/scss/variables';
 
   header {
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    Home
+    <div class="hero">
+      <img class="hero__img" src="../assets/img/hero-home-1.jpg" alt="" />
+      <div class="hero__text-wrapper">
+        <p class="hero__pre-title">Art Classes Start Now</p>
+        <h2 class="hero__title">What Starts Here,<br>Changes the World</h2>
+        <a class="hero__link" href="#">View upcoming classes <i class="icon-gizmo">chevron-right</i></a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +18,34 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+  @import '../assets/scss/variables';
 
+  .hero {
+    position: relative;
+
+    &__img {
+      width: 100%;
+    }
+    &__text-wrapper {
+      position: absolute;
+      top: 27%;
+      left: 10.5%;
+      color: white;
+    }
+    &__pre-title {
+      font-family: $font-primary;
+      text-transform: uppercase;
+      margin-bottom: 35px;
+      letter-spacing: 0.08em;
+    }
+    &__title {
+      font-family: $font-secondary;
+      font-size: 4.6em;
+      line-height: 0.9;
+    }
+    &__link {
+      font-family: $font-tertiary;
+    }
+  }
 </style>
