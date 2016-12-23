f9a6148be444d9d19e660ec5c1b76d1a22a2e180
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-12-22 23:24:34 -0800

    Added locations section but still need to finish up styles

---
+++ src/components/LocationCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="location__card">
+    <div class="location__card--top">
+      <div class="location__card--overlay">
+        <h2>Bonita</h2>
+      </div>
+    </div>
+    <div class="location__card--bottom">
+      <address>
+        4510 Bonita Road<br>
+        Bonita, CA 91902<br>
+        <a href="tel:+1-619-508-1299" rel="nofollow">(619) 508-1299</a>
+      </address>
+      <a href="mailto:bonita@artwithlarisse.com">Contact Us<i class="icon-gizmo">mail</i></a>
+      <a href="javascript();">Class Schedule<i class="icon-gizmo">calendar</i></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="home">
     <Hero :options="heroOptions" />
-    <div class="mission">
+    <section class="mission">
       <img class="mission__img" src="../assets/img/mission-artwork.png" alt="Flower and Kitten Artwork" />
       <p class="mission__text"><span class="mission__title">The Mission</span> We work hard to capture the unique style that each student brings while providing a fun and loving environment for students of all levels to grow and learn.</p>
-    </div>
+    </section>
+    <div class="spacer-secondary"></div>
+    <section class="location">
+      <span class="location__title">View Locations</span>
+      <div class="location__wrapper">
+        <div class="location__row">
+          <LocationCard />
+          <LocationCard />
+        </div>
+        <div class="location__row">
+          <LocationCard />
+          <LocationCard />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import Hero from '../components/Hero';
+import LocationCard from '../components/LocationCard';
 
 export default {
   name: 'Home',
-  components: { Hero },
+  components: { Hero, LocationCard },
   data() {
     return {
       heroOptions: {
@@ -32,6 +47,14 @@ export default {
 
   .hero {
     background-image: url('../assets/img/hero-home-1.jpg');
+  }
+  .spacer-secondary {
+    background-image: url('../assets/img/location-block-background.png');
+    height: 360px;
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    font-size: 0;
   }
   .mission {
     background-image: url('../assets/img/mission-block-background.png');
@@ -61,6 +84,52 @@ export default {
       font-weight: bold;
       transform: translateX(-130%);
       letter-spacing: 0.04em;
+    }
+  }
+  .location {
+    background-image: url('../assets/img/secondary-background-tile.png');
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+
+    &__title {
+      font-family: $font-secondary;
+      -webkit-font-smoothing: antialiased;
+      color: white;
+      font-size: 3.5em;
+      position: absolute;
+      top: -100px;
+    }
+    &__row {
+      display: flex;
+    }
+    &__card {
+      &:first-child {
+        margin-right: 30px;
+      }
+      &--top {
+        background-image: url('http://placekitten.com/475/250');
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+      &--overlay {
+        width: 475px;
+        height: 250px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba($brand-primary, .7);
+
+        h2 {
+          font-family: $font-secondary;
+          color: $brand-white;
+          font-size: 1.5em;
+          -webkit-font-smoothing: antialiased;
+        }
+      }
     }
   }
 </style>
